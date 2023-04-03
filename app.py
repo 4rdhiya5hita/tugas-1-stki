e239cd474a2bd072f4cf0bc8f73cac753bc287a9
John Doe <ardhiyashita2002@gmail.com>
2023-04-03 13:24:13 +0800

    update upload file dengan .txt

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
+import subprocess
+
 from flask import Flask, render_template, request
 from data_retrieval_text import data_retrieval_text
+from data_retrieval_txt import data_retrieval_txt
 
 app = Flask(__name__)
 
@@ -18,6 +21,37 @@ def data_result():
         input_text = request.form['input_text']
         result = data_retrieval_text(input_text)
         return render_template('result.html', result=result)    
+    
+@app.route('/txt', methods=['GET', 'POST'])
+def txt():
+    # if request.method == 'POST':
+    #     name = request.form['name']
+    #     data_retrieval_text(name)
+        # return f'Hello, {name}!'
+    
+    return render_template('data_txt.html')
+
+
+@app.route('/data_retrieval_txt', methods=['GET', 'POST'])
+def data_result_txt():
+    with open('example.txt', 'r') as file:
+        contents = file.read()
+        subprocess.call(['python', 'uji_data_retrieval_txt.py', contents])
+
+    # if request.method == 'POST':
+    #     # Get the uploaded file
+    #     file = request.files['file']
+
+    #     # read the contents of the file
+    #     contents = file.read()
+
+    #     # pass the contents as an argument to the other script
+    #     subprocess.call(['python', 'script2.py', contents])
+
+        # Read the contents of the file
+        # contents = file.read()
+        # result = data_retrieval_txt(file)
+        # return render_template('result_txt.html', result=result)    
 
 if __name__ == '__main__':
     app.run(debug=True)
--- uji_data_retrieval_txt.py
+++ uji_data_retrieval_txt.py
@@ -4,7 +4,7 @@ def data_retrieval_txt():
     # get the contents from the argument passed by Script 1
     contents = sys.argv[1]
 
-    # print(contents)
+    print(contents)
 
     # # Open the file in read mode
     # with open(file, 'r') as file:
@@ -12,6 +12,6 @@ def data_retrieval_txt():
     #     contents = file.read()
         
     # # Print the contents of the file
-    result = contents
-    return {'result': result}
+    # result = contents
+    # return {'result': result}
     # # print(contents)
