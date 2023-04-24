775e906cfb811bcc65d9710cac31db04f9822360
John Doe <ardhiyashita2002@gmail.com>
2023-04-24 15:45:27 +0800

    [UP] nambah tf-idf, weight, svm, cosine, and rank

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import subprocess
 from flask import Flask, render_template, request, send_file
 # from data_retrieval_text import data_retrieval_text
 # from data_retrieval_txt import data_retrieval_txt
-from data_retrieval import data_retrieval
+from data_retrieval import document_retrieval
 
 app = Flask(__name__)
 
@@ -19,9 +19,12 @@ def text():
 @app.route('/data_retrieval_text', methods=['GET', 'POST'])
 def data_result():
     if request.method == 'POST':
-        input_text = request.form['input_text']
-        result = data_retrieval(input_text)
-        return render_template('result_text.html', result=result)    
+        input_text1 = request.form['input_text1']
+        input_text2 = request.form['input_text2']
+        input_text3 = request.form['input_text3']
+        search_word = request.form['search_word']
+        result = document_retrieval(input_text1, input_text2, input_text3, search_word)
+        return render_template('result_text.html', result=result, search_word=search_word)    
     
 
 @app.route('/txt', methods=['GET'])
@@ -32,16 +35,21 @@ def txt():
 @app.route('/data_retrieval_txt', methods=['GET', 'POST'])
 def data_result_txt():
     if request.method == 'POST':
-        file = request.files['myfile']
-        file_contents = file.read() # read the content
+        file1 = request.files['myfile1']
+        file_contents1 = file1.read() # read the content
+        input_text1 = file_contents1.decode('utf-8') # To convert the bytes to a string
         
-        # To convert the bytes to a string
-        input_text = file_contents.decode('utf-8')
-        result = data_retrieval(input_text)
+        file2 = request.files['myfile2']
+        file_contents2 = file2.read()
+        input_text2 = file_contents2.decode('utf-8')
+
+        search_word = request.form['search_word']
+        
+        result = document_retrieval(input_text1, input_text2, search_word)
         # print(result)
 
         # return file_contents
-        return render_template('result_txt.html', result=result)    
+        return render_template('result_txt.html', result=result, search_word=search_word)
 
 
 @app.route('/', methods=['GET'])
--- data_retrieval.py
+++ data_retrieval.py
@@ -1,4 +1,8 @@
 import string
+import math
+import numpy as np
+from scipy.stats import rankdata
+
 # Example using nltk for data retrieval purpose
 import nltk
 nltk.download('stopwords')
@@ -8,6 +12,161 @@ from nltk.tokenize import word_tokenize
 nltk.download('punkt')
 nltk.download('wordnet')
 from nltk.stem import WordNetLemmatizer, PorterStemmer
+
+def document_retrieval (input_doc1, input_doc2, input_doc3, search_word):
+    # arr_doc1 = []
+    # arr_doc2 = []
+    arr_doc1 = data_retrieval(input_doc1)
+    arr_doc2 = data_retrieval(input_doc2)
+    arr_doc3 = data_retrieval(input_doc3)
+    token_array = list(set(arr_doc1 + arr_doc2 + arr_doc3))
+    # token_array = arr.array('u', arr_doc)
+
+    q = []
+    d1 = []
+    d2 = []
+    d3 = []
+
+    for word in token_array:
+        if word == search_word:
+            q.append(1)
+        else:
+            q.append(0)
+
+        if word in arr_doc1:
+            d1.append(1)
+        else:
+            d1.append(0)
+
+        if word in arr_doc2:
+            d2.append(1)
+        else:
+            d2.append(0)
+
+        if word in arr_doc3:
+            d3.append(1)
+        else:
+            d3.append(0)
+
+            # match_word.append(word)
+
+    df = []
+    D = []
+    log = []
+    w_q = []
+    w_d1 = []
+    w_d2 = []
+    w_d3 = []
+    v_q = []
+    v_d1 = []
+    v_d2 = []
+    v_d3 = []
+    vsm_d1 = []
+    vsm_d2 = []
+    vsm_d3 = []
+    cos_document = []
+    data_vector_Wq = 0
+    i = 0
+    while i < len(token_array):
+        sum_d = int(d1[i]) + int(d2[i]) + int(d3[i])
+        D_df = 3/sum_d
+        idf_log = format(math.log10(D_df), ".3f")
+
+        if q[i] == 0:
+            Wq = "0"
+            vector_Wq = "0"
+        else:
+            Wq = idf_log*q[i]
+            vector_Wq = format(pow(float(Wq), 2), ".3f")
+            data_vector_Wq = vector_Wq
+
+        if d1[i] == 0:
+            W1 = "0"
+            vector_W1 = "0"
+            vsm_W1 = "0"
+        else:
+            W1 = idf_log*d1[i]
+            vector_W1 = format(pow(float(W1), 2), ".3f")
+            vsm_W1 = format(float(data_vector_Wq)*float(vector_W1), ".5f")
+
+        if d2[i] == 0:
+            W2 = "0"
+            vector_W2 = "0"
+            vsm_W2 = "0"
+        else:
+            W2 = idf_log*d2[i]
+            vector_W2 = format(pow(float(W2), 2), ".3f")
+            vsm_W2 = format(float(data_vector_Wq)*float(vector_W2), ".5f")
+
+        if d3[i] == 0:
+            W3 = "0"
+            vector_W3 = "0"
+            vsm_W3 = "0"
+        else:
+            W3 = idf_log*d3[i]
+            vector_W3 = format(pow(float(W3), 2), ".3f")
+            vsm_W3 = format(float(data_vector_Wq)*float(vector_W3), ".5f")
+        
+        df.append(sum_d)
+        D.append(D_df)
+        log.append(idf_log)
+        w_q.append(Wq)
+        w_d1.append(W1)
+        w_d2.append(W2)
+        w_d3.append(W3)
+        v_q.append(vector_Wq)
+        v_d1.append(vector_W1)
+        v_d2.append(vector_W2)
+        v_d3.append(vector_W3)
+        vsm_d1.append(vsm_W1)
+        vsm_d2.append(vsm_W2)
+        vsm_d3.append(vsm_W3)
+
+        i += 1
+
+    sqrt_q = format(pow(sum([float(x) for x in v_q]), 1/2), ".3f")
+    sqrt_d1 = format(pow(sum([float(x) for x in v_d1]), 1/2), ".3f")
+    sqrt_d2 = format(pow(sum([float(x) for x in v_d2]), 1/2), ".3f")
+    sqrt_d3 = format(pow(sum([float(x) for x in v_d3]), 1/2), ".3f")
+
+    sum_vsm_d1 = format(sum([float(x) for x in vsm_d1]), ".4f")
+    sum_vsm_d2 = format(sum([float(x) for x in vsm_d2]), ".4f")
+    sum_vsm_d3 = format(sum([float(x) for x in vsm_d3]), ".4f")
+
+    cos_d1 = format(float(sum_vsm_d1) / (float(sqrt_q) * float(sqrt_d1)), ".4f")
+    cos_d2 = format(float(sum_vsm_d2) / (float(sqrt_q) * float(sqrt_d2)), ".4f")
+    cos_d3 = format(float(sum_vsm_d3) / (float(sqrt_q) * float(sqrt_d3)), ".4f")
+
+    cos_document.append(cos_d1)
+    cos_document.append(cos_d2)
+    cos_document.append(cos_d3)    
+    cos_rank = len(cos_document) - rankdata(cos_document, method='average') + 1
+    rank = [int(x) for x in cos_rank]
+
+    valid_search_word = search_word.lower()
+    count1 = 0
+    for word in arr_doc1:
+        if valid_search_word == word:
+            count1 += 1
+
+    count2 = 0
+    for word in arr_doc2:
+        if valid_search_word == word:
+            count2 += 1
+    
+    count3 = 0
+    for word in arr_doc3:
+        if valid_search_word == word:
+            count3 += 1
+    
+    
+
+    return {'arr_doc1': arr_doc1, 'arr_doc2': arr_doc2, 'arr_doc3': arr_doc3, 'count1': count1, 'count2': count2, 'count3': count3, 'search_word': search_word, 
+            'token_array': token_array, 'q': q, 'd1': d1, 'd2': d2, 'd3': d3, 'df': df, 'D': D, 'log': log, 'w_q': w_q, 'w_d1': w_d1, 'w_d2': w_d2, 'w_d3': w_d3,
+            'v_q': v_q, 'v_d1': v_d1, 'v_d2': v_d2, 'v_d3': v_d3, 'sqrt_q': sqrt_q, 'sqrt_d1': sqrt_d1, 'sqrt_d2': sqrt_d2, 'sqrt_d3': sqrt_d3,
+            'vsm_d1': vsm_d1, 'vsm_d2': vsm_d2, 'vsm_d3': vsm_d3, 'sum_vsm_d1': sum_vsm_d1, 'sum_vsm_d2': sum_vsm_d2, 'sum_vsm_d3': sum_vsm_d3,
+            'cos_d1': cos_d1, 'cos_d2': cos_d2, 'cos_d3': cos_d3, 'cos_document': cos_document, 'cos_rank': rank}    
+
 
 def data_retrieval(input_text):
     # Create a stop word list for English
@@ -55,4 +214,5 @@ def data_retrieval(input_text):
 
     # print(final_sentence)
     # , 'porter_sentence': porter_sentence, 'wnl_sentence': wnl_sentence
-    return {'final_sentence': final_sentence, "input_text": input_text}
+    # return {'final_sentence': final_sentence, "input_text": input_text}
+    return without_punctuation
